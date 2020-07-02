5123a1448da92d330dfb6ad281667c2e09cc6358
samedamci <samedamci@disroot.org>
2020-07-02 13:57:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# nyaa.si API
+
+Unofficial [nyaa.si](https://nyaa.si) HTTP API based on scrapping [NyaaPy Python library](https://github.com/JuanjoSalvador/NyaaPy).
+
+## Usage
+
++ All queries must be a GET requests.
+
+### Example request with cURL
+```
+$ curl "http://localhost:5000/?q=pokemon&category=2&subcategory=3"
+```
+### Parameters
+Parameter | Value
+:-- | :--
+q | query string (required)
+category | [category number](#categories) (optional, default 0)
+subcategory | [subcategory number](#categories) (optional, default 0)
+page | page number (optional, default 1, range: 0-1000)
+
+### Categories
+Number | Category
+:--- | :---
+0 | All categories and subcategories
+1 | Anime
+2 | Audio
+3 | Literature
+4 | Live Action
+5 | Pictures
+6 | Software
+
+### Subcategories
+Number (Cat.Sub) | Subcategory
+:--- | :---
+1.1 | Anime Music Video
+1.2 | English-translated
+1.3 | Non-English-translated
+1.4 | Raw
+2.1 | Lossless
+2.2 | Lossy
+3.1 | English-translated
+3.2 | Non-English-translated
+3.3 | Raw
+4.1 | English-translated
+4.2 | Idol/Promotional Video
+4.3 | Non-English-translated
+4.4 | Raw
+5.1 | Graphics
+5.2 | Photos
+6.1 | Applications
+6.2 | Games
+
+## Hosting
+
++ Initialize virtualenv.
++ Install requirements.
++ Run `main.py` with `python3 main.py`.
---
+++ main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+from flask import Flask, request, jsonify
+from NyaaPy import Nyaa
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def api():
+    if "q" in request.args:
+        if "category" in request.args:
+            try:
+                category = int(request.args["category"])
+            except ValueError:
+                return "Category argument must be integer number."
+        else:
+            category = 0
+
+        if "subcategory" in request.args:
+            try:
+                subcategory = int(request.args["subcategory"])
+            except ValueError:
+                return "Subategory argument must be integer number."
+        else:
+            subcategory = 0
+
+        if "page" in request.args:
+            try:
+                page = int(request.args["page"])
+                if page > 1000 or page < 0:
+                    return "Page must be a integer number between 0 and 1000."
+            except ValueError:
+                return "Page argument must be integer number."
+        else:
+            page = 1
+
+        return jsonify(
+            Nyaa.search(
+                keyword=request.args["q"],
+                category=category,
+                subcategory=subcategory,
+                page=page,
+            )
+        )
+    else:
+        return "Please specify required query argument."
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
